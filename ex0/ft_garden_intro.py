c8dd4029835cec68ee331c039feca7096bb0aa81
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-06-13 13:18:17 +0200

    Write required function

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Pyhton module that prints a series of variables with set values
+
+if __name__ == "__main__":
+    plant_name: str = "Rose"
+    plant_heigth: int = 25
+    plant_age: int = 30
+    print(f"=== Welcome to My Garden ===")
+    print(f"Plant: {plant_name}")
+    print(f"Height: {plant_heigth}cm")
+    print(f"Age: {plant_age} days")
