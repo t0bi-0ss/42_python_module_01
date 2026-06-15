cbc233abcdf0b67bb44e44311fa86c9e77bfdb1e
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-15 22:41:24 +0200

    Correct wrong typehint

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -7,7 +7,7 @@ Demonstrates creating and displaying multiple plants.
 class Plant:
     """Represents a plant with basic growth metrics."""
 
-    def __init__(self, name: str, height: int = 0, age: int = 0) -> None:
+    def __init__(self, name: str, height: float = 0, age: int = 0) -> None:
         """Initialize a plant with name, height (cm), and age (days)."""
         if height >= 0 and age >= 0 and name != "":
             self.name = name
