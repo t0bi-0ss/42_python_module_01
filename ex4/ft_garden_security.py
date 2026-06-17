0d12945b663f73c5abba8949837698e7ebc0f49e
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-17 21:25:28 +0200

    Update Plant class with corrected one from ex3

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -1,12 +1,20 @@
 class Plant:
     """Represents a plant with basic growth metrics."""
 
-    def __init__(self, name: str, height: float = 0, age: int = 0) -> None:
-        """Initialize a plant with name, height (cm), and age (days)."""
+    def __init__(
+        self,
+        name: str,
+        height: float = 0,
+        age: int = 0,
+        growth_rate: float = 0
+    ) -> None:
+        """Initialize a plant with name, height (cm), age
+        (days) and growth rate (cm)."""
         if height >= 0 and age >= 0 and name != "":
             self.__name = name
             self.__height = height
             self.__age = age
+            self.__growth_rate = growth_rate
 
     def get_name(self) -> str:
         return self.__name
@@ -17,21 +25,34 @@ class Plant:
     def get_age(self) -> int:
         return self.__age
 
+    def get_growth_rate(self) -> float:
+        return self.__growth_rate
+    
+    def set_growth_rate(self, growth_rate: float) -> None:
+        """Set plant's growth rate (cm) per day"""
+        if growth_rate <= 0:
+            print("Error: growth rate must be a positive number")
+            return
+        self.__growth_rate = growth_rate
+
     def show(self) -> None:
         """Print the plant's information."""
         print(f"{self.__name.capitalize()}: {self.__height:.1f}cm, ", end="")
         print(f"{self.__age} days old")
 
     def grow(self) -> None:
-        """Increment plant's heigth by .8cm"""
-        self.__height += 0.8
+        """Increment plant's heigth by growth rate"""
+        if self.growth_rate == 0:
+            print("Error: growth_rate has not been set yet")
+            return
+        self.__height += self.growth_rate
 
-    def days_passed(self, days: int = 1) -> None:
-        """Grow plant according to days passed"""
-        if days > 0:
-            self.__age += days
-            for day in range(1, days + 1):
-                self.grow()
+    def age_plant(self, days: int = 1) -> None:
+        """Modify plant's age by days passed"""
+        if days <= 0:
+            print("Error: days must be a positive number")
+            return
+        self.__age += days
 
     def set_height(self, new_height: int) -> None:
         if new_height < 0:
