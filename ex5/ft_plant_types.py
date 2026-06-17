72d8b2c3b471741f162e51d4ba35ec97705d9e3c
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-17 21:26:23 +0200

    Update Plant class with corrected one from ex3

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -1,14 +1,20 @@
 class Plant:
     """Represents a plant with basic growth metrics."""
 
-    def __init__(self, name: str, height: float = 0, age: int = 0) -> None:
-        """Initialize a plant with name, height (cm), and age (days)."""
+    def __init__(
+        self,
+        name: str,
+        height: float = 0,
+        age: int = 0,
+        growth_rate: float = 0
+    ) -> None:
+        """Initialize a plant with name, height (cm), age
+        (days) and growth rate (cm)."""
         if height >= 0 and age >= 0 and name != "":
             self.__name = name
             self.__height = height
             self.__age = age
-        else:
-            print("Error: could not initiate class with passed values")
+            self.__growth_rate = growth_rate
 
     def get_name(self) -> str:
         return self.__name
@@ -19,33 +25,34 @@ class Plant:
     def get_age(self) -> int:
         return self.__age
 
+    def get_growth_rate(self) -> float:
+        return self.__growth_rate
+    
+    def set_growth_rate(self, growth_rate: float) -> None:
+        """Set plant's growth rate (cm) per day"""
+        if growth_rate <= 0:
+            print("Error: growth rate must be a positive number")
+            return
+        self.__growth_rate = growth_rate
+
     def show(self) -> None:
         """Print the plant's information."""
         print(f"{self.__name.capitalize()}: {self.__height:.1f}cm, ", end="")
         print(f"{self.__age} days old")
 
     def grow(self) -> None:
-        """Increment plant's heigth by .8cm"""
-        self.__height += 0.8
+        """Increment plant's heigth by growth rate"""
+        if self.growth_rate == 0:
+            print("Error: growth_rate has not been set yet")
+            return
+        self.__height += self.growth_rate
 
-    def days_passed(self, days: int = 1) -> None:
-        """Grow plant according to days passed"""
-        if days > 0:
-            self.__age += days
-            for day in range(1, days + 1):
-                self.grow()
-
-    def garden_plant_growth(self, days: int = 0) -> None:
-        """Display plant's growth during a given number of days"""
-        original_height = self.__height
-        if days > 0:
-            print("=== Garden Plant Growth ===")
-            self.show()
-            for day in range(1, days + 1):
-                self.days_passed()
-                print(f"=== Day {day} ===")
-                self.show()
-            print(f"Growth this week: {(self.__height - original_height):.1f}")
+    def age_plant(self, days: int = 1) -> None:
+        """Modify plant's age by days passed"""
+        if days <= 0:
+            print("Error: days must be a positive number")
+            return
+        self.__age += days
 
     def set_height(self, new_height: int) -> None:
         if new_height < 0:
@@ -189,8 +196,21 @@ class Vegetable(Plant):
             return
         self.__nutritional_value = nutritional_value
 
+    def get_harvest_season(self) -> str:
+        return self.__harvest_season
     
+    def set_harvest_season(self, harvest_season: str) -> None:
+        if harvest_season == "":
+            print("Error: harvest season must not be an empty string")
+            return
+        self.__harvest_season = harvest_season
 
+    def show(self) -> None:
+        super().show()
+        print(f"Harvest season: {self.__harvest_season}")
+        print(f"Nutritional value: {self.__nutritional_value}")
+
+    
 
 if __name__ == "__main__":
     print("=== Garden Plant Types ===")
